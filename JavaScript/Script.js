8d265a973c3f773976b78776780545d22041adaa
Anthony Garcia <anthonyjaemes98@gmail.com>
2023-12-08 15:32:22 -0800

    Added jQuery to bring page to top on refresh.

--- JavaScript/Script.js
+++ JavaScript/Script.js
@@ -2,23 +2,23 @@
 //     window.scrollTo(0, 0);
 // }
 
-// $(document).ready(function () {
-//     window.scrollTo(0,0);
-// });
+$(document).ready(function () {
+    window.scrollTo(0,0);
+});
 
 
-// window.onload=function() {
-//     window.scrollTo(0, 0);
-//     const dropBtn = document.querySelector(".dropbtn");
-//     dropBtn.addEventListener("click", showMenu);
+window.onload=function() {
+    // window.scrollTo(0, 0);
+    const dropBtn = document.querySelector(".dropbtn");
+    dropBtn.addEventListener("click", showMenu);
 
-//     function showMenu() {
-//         const menu = document.querySelector(".dropdown-content");
-//         if(menu.style.display == "block") {
-//             menu.style.display = "none";
-//         }
-//         else {
-//             menu.style.display = "block";
-//         }
-//     }
-// }
+    function showMenu() {
+        const menu = document.querySelector(".dropdown-content");
+        if(menu.style.display == "block") {
+            menu.style.display = "none";
+        }
+        else {
+            menu.style.display = "block";
+        }
+    }
+}
