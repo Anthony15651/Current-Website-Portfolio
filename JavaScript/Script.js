edff66365726adc61052a57d5799e7c86cc2e6c0
Anthony Garcia <anthonyjaemes98@gmail.com>
2023-12-08 15:28:36 -0800

    Adding fade in effect for loading

--- JavaScript/Script.js
+++ JavaScript/Script.js
@@ -1,14 +1,24 @@
-window.onload=function() {
-    const dropBtn = document.querySelector(".dropbtn");
-    dropBtn.addEventListener("click", showMenu);
+// window.onbeforeunload = function () {
+//     window.scrollTo(0, 0);
+// }
 
-    function showMenu() {
-        const menu = document.querySelector(".dropdown-content");
-        if(menu.style.display == "block") {
-            menu.style.display = "none";
-        }
-        else {
-            menu.style.display = "block";
-        }
-    }
-}
+// $(document).ready(function () {
+//     window.scrollTo(0,0);
+// });
+
+
+// window.onload=function() {
+//     window.scrollTo(0, 0);
+//     const dropBtn = document.querySelector(".dropbtn");
+//     dropBtn.addEventListener("click", showMenu);
+
+//     function showMenu() {
+//         const menu = document.querySelector(".dropdown-content");
+//         if(menu.style.display == "block") {
+//             menu.style.display = "none";
+//         }
+//         else {
+//             menu.style.display = "block";
+//         }
+//     }
+// }
