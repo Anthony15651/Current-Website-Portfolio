fa91fad511ee8ab5f77ffcfca6cd672f955aa626
Anthony Garcia <anthonyjaemes98@gmail.com>
2023-12-08 14:51:49 -0800

    Adding JS for dropdown menu on smaller screens.

---
+++ JavaScript/Script.js
@@ -0,0 +1,14 @@
+window.onload=function() {
+    const dropBtn = document.querySelector(".dropbtn");
+    dropBtn.addEventListener("click", showMenu);
+
+    function showMenu() {
+        const menu = document.querySelector(".dropdown-content");
+        if(menu.style.display == "block") {
+            menu.style.display = "none";
+        }
+        else {
+            menu.style.display = "block";
+        }
+    }
+}
